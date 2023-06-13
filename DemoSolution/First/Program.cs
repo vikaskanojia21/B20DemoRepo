136605cb3688b3129e82b25980e667da8b095557
vikaskanojia21 <vikaskanojia21@gmail.com>
2023-06-13 18:16:25 +0530

    Initail update2

--- DemoSolution/First/Program.cs
+++ DemoSolution/First/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Paresh Jadiya");
+
+Console.WriteLine("paresh black Bhais aahe");
 Console.ReadLine();
