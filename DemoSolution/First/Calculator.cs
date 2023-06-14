26ffafc6902f4201e1571c2756dfc034de5fee66
vikaskanojia21 <vikaskanojia21@gmail.com>
2023-06-14 11:20:07 +0530

    Added add method with new calculator class

---
+++ DemoSolution/First/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    public class Calculator
+    {
+
+        public void Addition(int a, int b)
+        {
+            Console.WriteLine($"Addition of two numbers {a} + {b}={a+b}");
+        }
+    }
+}
